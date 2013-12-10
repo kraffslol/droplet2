3a8ad3d0c820ee0b85cb0d0845a86fa535ea5c28
Laurent Van Winckel <laurentvw@gmail.com>
2013-12-10 21:43:27 +0100

    App boilerplate code. Small structure changes.

--- droplet-server/app.js
+++ droplet-server/app.js
@@ -1,5 +1,39 @@
-var express = require('express');
-var app = express();
+/**
+ * Module dependencies
+ */
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var express = require('express'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+
+/**
+ * Routes
+ */
+
+app.post('/upload', function(req, res) {
+  // TO DO: upload route
+});
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
